d12e5aa7fa04fec45a53eabff16ce0734f76d12b
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2022-11-04 15:45:18 +0000

    Save a few bytes by refactoring

--- beeb/varcode.asm
+++ beeb/varcode.asm
@@ -3,20 +3,21 @@
   ; First check if there is any water
   LDA noofwater:BEQ done
 
+  ; Save water count for loop
   TAX
-  LDA #lo(waterlist):STA zptr4
-  LDA #hi(waterlist):STA zptr4+1  
 
   ; Get water colour, e.g. water or lava
-  LDA watercolour:AND #&E7:ORA #&10:STA frmattri
+  LDA watercolour
+  AND #&E7:ORA #&10 ; Make sure it's set to EOR plot
+  STA frmattri
 
   LDY #&00
 .updatewaterlp
 
-  LDA (zptr4), Y:STA frmx:INY
-  LDA (zptr4), Y:STA frmy:INY
-  LDA (zptr4), Y
-  STA zidx4:INC zidx4:LDA zidx4:AND #&03:STA (zptr4), Y:INY
+  LDA waterlist, Y:STA frmx:INY
+  LDA waterlist, Y:STA frmy:INY
+  LDA waterlist, Y
+  STA zidx4:INC zidx4:LDA zidx4:AND #&03:STA waterlist, Y:INY
   CLC:ADC #92:STA frmno
 
   JSR frame
@@ -34,19 +35,19 @@
   ; First check if there are flames
   LDA noofflames:BEQ done
 
+  ; Save flame count for loop
   TAX
-  LDA #lo(flamelist):STA zptr4
-  LDA #hi(flamelist):STA zptr4+1
 
   ; Update flame loop
   LDA #115:STA frmno
-  LDA clock:AND #&01:TAY:LDA flamecolours, Y:STA frmattri
+  LDA clock:AND #&01:TAY:LDA flamecolours, Y ; Set flame colour based on odd/even clock
+  STA frmattri
 
   LDY #&00
 .updateflamelp
 
-  LDA (zptr4), Y:STA frmx:INY
-  LDA (zptr4), Y:STA frmy:INY
+  LDA flamelist, Y:STA frmx:INY
+  LDA flamelist, Y:STA frmy:INY
   INY
   JSR frame
 
