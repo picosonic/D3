2a72577ee90e346edfa8e072d9489c4cc9424cbf
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2023-02-06 15:31:45 +0000

    No need to mask attributes

--- beeb/objects.asm
+++ beeb/objects.asm
@@ -825,7 +825,7 @@ noofmoving = (endofmovingdata-movingdata)/movingsize
   LDY #movefrm:LDA (zptr4), Y:STA frmno
   LDY #movex:LDA (zptr4), Y:STA frmx
   LDY #movey:LDA (zptr4), Y:STA frmy
-  LDY #colour:LDA (zptr4), Y:AND %01000111:STA frmattri
+  LDY #colour:LDA (zptr4), Y:STA frmattri
 
   JSR frame
 
